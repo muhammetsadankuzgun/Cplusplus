26be104f24fb124c8653eacad4fe9e605e6fc30f
Muhammet Şadan Kuzgun <100411750+muhammetsadankuzgun@users.noreply.github.com>
2022-12-12 13:35:51 +0300

    add and multiplication example
    I did add and multiplecation in c++ app.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    std::cout<<"First number:"<<std::endl;
+    
+    std::cin>>a;
+    std::cout<<"Second number:"<<std::endl;
+    
+    std::cin>>b;
+    
+    std::cout<<"result:"<<a+b<<std::endl;
+    std::cout<<"result:2"<<a*b;
+    
+}
