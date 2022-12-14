0de8464a37c4817b0ecdc3a8adabe0b8750167c8
Muhammet Şadan Kuzgun <100411750+muhammetsadankuzgun@users.noreply.github.com>
2022-12-14 12:41:07 +0300

    if, else
    simple example about "if,else"

---
+++ + symbol.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    char sym = '+';
+    
+    std::cout<<"Enter first number:"<<std::endl;
+    std::cin>>a;
+    std:cout<<"Enter second Number:"<<std::endl;
+    std::cin>>b;
+    std::cout<<"Please take an action:"<<std::endl;
+    std::cin>> sym;
+    
+    if(a>=99){
+        std::cout<<"Result=> "<<a+b;
+        
+    }
+    else if (a<98){
+        std::cout<<"Other Result=> "<<a+b;
+    
+    }
+    else {
+    std::cout <<"Yarragı yediniz";
+
+
+}
+}
