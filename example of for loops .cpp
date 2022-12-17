9ba8894bb5bcc98fc25353f4545066db2a3e3a17
Muhammet Şadan Kuzgun <100411750+muhammetsadankuzgun@users.noreply.github.com>
2022-12-17 20:40:06 +0300

    for loop
    I had a simple example about "for" loop.

---
+++ example of for loops .cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i=0; i<2023; i=i+2)
+    {
+    std::cout<<"Level : "<<i<<std::endl;
+}
+  std::cout<<"***Welcome 2023***"<<std::endl;
+}
